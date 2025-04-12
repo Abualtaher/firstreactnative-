c837b148b491163d2d49eb1c56e60f4ee221062b
Abualtaher <wisamaltaher@gmail.com>
2025-04-12 22:20:39 +0200

    worked on my first page

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,28 +1,40 @@
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import appImage from "@/assets/images/Second.jpg";
 
-import appImage from "./assets/images/Second.jpg";
-
-const app = () => {
+const App = () => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={appImage} style={styles.container}>
-        <Text style={styles.text}>Coffe Shop </Text>
+      <ImageBackground
+        source={appImage}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
       </ImageBackground>
     </View>
   );
 };
 
-export default app;
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
   },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   text: {
     color: "white",
     fontWeight: "bold",
     fontSize: 42,
     textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    padding: 10,
   },
 });
